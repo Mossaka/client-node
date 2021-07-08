65c8e6a846776a27159505e4db4f54d9220465ff
Jiaxiao Zhou <jiazho@microsoft.com>
2021-07-07 22:05:14 -0700

    getting rid of the queue.send in clients

--- src/utils.rs
+++ src/utils.rs
@@ -3,9 +3,9 @@ use std::{sync::Arc, u32};
 use neon::prelude::*;
 
 use std::ops::Bound;
-use tikv_client::{Key, KvPair, TimestampExt};
+use tikv_client::{request::KvRequest, Key, KvPair, TimestampExt};
 
-use crate::{RawClient, Transaction, TransactionClient};
+use crate::{RawClient, Snapshot, Transaction, TransactionClient};
 use lazy_static::lazy_static;
 use tokio::{runtime::Runtime, sync::Mutex};
 
@@ -36,6 +36,18 @@ impl ToJS for () {
 impl ToJS for Vec<Key> {
     fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
         rust_keys_to_js_array(cx, self).upcast()
+    }
+}
+
+impl ToJS for Key {
+    fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
+        bytes_to_js_string(cx, self.into())
+    }
+}
+
+impl ToJS for tikv_client::Value {
+    fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
+        bytes_to_js_string(cx, self.into())
     }
 }
 
@@ -72,6 +84,15 @@ impl ToJS for tikv_client::Transaction {
     }
 }
 
+impl ToJS for tikv_client::Snapshot {
+    fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
+        cx.boxed(Snapshot {
+            inner: Arc::new(Mutex::new(self)),
+        })
+        .upcast()
+    }
+}
+
 impl<T: ToJS> ToJS for Option<T> {
     fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
         match self {
@@ -84,6 +105,12 @@ impl<T: ToJS> ToJS for Option<T> {
 impl ToJS for tikv_client::Timestamp {
     fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
         cx.number(self.version() as f64).upcast()
+    }
+}
+
+impl ToJS for bool {
+    fn to_js_value<'a>(self, cx: &mut TaskContext<'a>) -> Handle<'a, JsValue> {
+        cx.boolean(self).upcast()
     }
 }
 
@@ -109,12 +136,8 @@ pub fn rust_pairs_to_js_array<'a>(
 
 pub fn rust_keys_to_js_array<'a>(cx: &mut TaskContext<'a>, keys: Vec<Key>) -> Handle<'a, JsArray> {
     let js_array = JsArray::new(cx, keys.len() as u32);
-    for (i, obj) in keys.iter().enumerate() {
-        let v1 = cx.string(
-            std::str::from_utf8(&Vec::from(obj.clone()))
-                .unwrap()
-                .to_owned(),
-        );
+    for (i, obj) in keys.into_iter().enumerate() {
+        let v1 = obj.to_js_value(cx);
         js_array.set(cx, i as u32, v1).unwrap();
     }
     js_array
